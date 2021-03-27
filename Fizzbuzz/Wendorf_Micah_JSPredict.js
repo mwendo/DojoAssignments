10b26975e823caea2157cb6acab4e390c75fb9f3
Micah Wendorf <wendorfmicah0@gmail.com>
2021-03-27 14:03:14 -0500

    initial commit

---
+++ Fizzbuzz/Wendorf_Micah_JSPredict.js
@@ -0,0 +1,30 @@
+// var msg = 'codingdojo';
+// for(var x=1; x<msg.length-2; x++)
+// {
+//     if(msg.length == 3)
+//     {
+//         for (var i=1; i<7; i++)
+//         {
+//             console.log(i);
+//         }
+//     }
+//     else
+//     {
+//         for(var i=msg.length; i>(msg.length - 2); i--)
+//         {
+//             console.log(i);
+//         }
+//     }
+// }
+
+for (var i=0;i<=100;i++) {
+    if(i%3==0) {
+        console.log("Fizz")
+    }
+    if (i%==0) {
+        console.log("Buzz")
+    }
+    if(i%3&&i%5) {
+        
+    }
+}
