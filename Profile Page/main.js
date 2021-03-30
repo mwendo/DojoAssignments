469634fd8529591c9997fd6673755e32b259fe13
Micah Wendorf <wendorfmicah0@gmail.com>
2021-03-30 13:01:20 -0500

    added js interactivity

---
+++ Profile Page/main.js
@@ -0,0 +1,15 @@
+var differentName = "Naruto Uzumaki";
+var change = document.querySelector("#name");
+
+function changeName () {
+    differentName;
+    change.innerText = differentName;
+    console.log(differentName);
+}
+
+function myFunction1() { 
+	document.getElementById("mainFrameOne").style.display="none"; 
+}
+function myFunction2() {
+    document.getElementById("mainFrameTwo").style.display="none";
+}
